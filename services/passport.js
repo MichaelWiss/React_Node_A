a268fe371d774a69afa324ef9a51be78b92c0448
MichaelWiss <michael.wiss@gmail.com>
2017-08-01 11:06:33 -0500

    check for user id

--- services/passport.js
+++ services/passport.js
@@ -14,9 +14,15 @@ passport.use(
 	  callbackURL: '/auth/google/callback'
   }, 
    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ })
-      
-      new User({ googleId: profile.id }).save();
-   }
+      User.findOne({ googleId: profile.id })
+       .then((existingUser) => {
+        if (existingUser) {
+
+        } else {
+        
+        new User({ googleId: profile.id }).save();
+        }
+      })
+    }
   )
 );
