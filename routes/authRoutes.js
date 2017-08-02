a481af1fe23bd620b3d7f9a9973939aa69894684
MichaelWiss <michael.wiss@gmail.com>
2017-08-02 14:13:43 -0500

    using session

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,9 +13,11 @@ app.get(
 	);
 app.get('/api/logout', (req, res) => {
    req.logout();
+   res.send(req.user);
 });
 
 app.get('/api/current_user', (req, res) => {
-    res.send(req.user);
+	res.send(req.session);
+    // res.send(req.user);
   });
 };
