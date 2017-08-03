2e61bd3ccfb2b25bef90779c38a6a06d5a602c98
MichaelWiss <michael.wiss@gmail.com>
2017-08-02 21:37:15 -0500

    dev or prod

---
+++ config/dev.js
@@ -0,0 +1,16 @@
+//dev.js
+
+module.exports = {
+	googleClientID: '1015889727661-ib28rije4a4rfclu0c1lfj6mrh5h4nus.apps.googleusercontent.com',
+    googleClientSecret: 'OcXOsvpkXJUxi00zzSmWnnwF',
+    mongoURI: 'mongodb://72_Knisho:password@ds129023.mlab.com:29023/emaily--dev',
+    cookieKey: 'luablfbalkdbafdkbfadbsfhlgbflshgfbgalfhfuiew38yr894bf'
+};
+
+
+module.exports = {
+	googleClientID: '363287999405-2dtq3glkgr8dvmgmf1p6iqkn99j2eqo5.apps.googleusercontent.com',
+	googleClientSecret: 'IUJU0ai3_dyD4369ZQ2EVo-P',
+	mongoURI:
+	cookieKey:
+}
