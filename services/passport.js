37282cc0acd9bb30e9a2afa785f4810b729b2e00
MichaelWiss <michael.wiss@gmail.com>
2017-08-03 20:28:44 -0500

    using async

--- services/passport.js
+++ services/passport.js
@@ -25,15 +25,15 @@ passport.use(
 	  callbackURL: '/auth/google/callback',
     proxy: true
   }, 
-   (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id })
+   async (accessToken, refreshToken, profile, done) => {
+     await  User.findOne({ googleId: profile.id })
        .then((existingUser) => {
         if (existingUser) {
 
         done(null, existingUser);
         } else {
         
-        new User({ googleId: profile.id })
+       await new User({ googleId: profile.id })
          .save()
          .then(user => done(null, user));
         }
