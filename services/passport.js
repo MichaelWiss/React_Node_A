5d902b4bcde4ca2eb64c69ea6dfc1be949693bde
MichaelWiss <michael.wiss@gmail.com>
2017-08-07 18:24:30 -0500

    refactor

--- services/passport.js
+++ services/passport.js
@@ -26,14 +26,14 @@ passport.use(
     proxy: true
   }, 
    async (accessToken, refreshToken, profile, done) => {
-     const existingUser = await User.findOne({ googleId: profile.id })
-       if (existingUser) {
-       done(null, existingUser);
-      } else {
+     const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+       return done(null, existingUser);
+      } 
         
-       const user = await new User({ googleId: profile.id }).save()
-         done(null, user);
+      const user = await new User({ googleId: profile.id }).save()
+      done(null, user);
       }
-    }
-  )
+    )
 );
