7537c50d1f82a46ad00ecb753e937e73f27f82a9
MichaelWiss <michael.wiss@gmail.com>
2017-08-10 12:00:54 -0500

    exact

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,7 +4,7 @@ import {BrowserRouter, Route } from 'react-router-dom';
 const Header = () => <h2>Header</h2>
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
-const Landing = () => <h2>Landing</h2>
+const Landing = () => <h2>Landing Hello There</h2>
 
 const App = () => {
 	return (
@@ -12,7 +12,9 @@ const App = () => {
          
         <BrowserRouter>
         <div>
-          <Route path="/" component={Landing} />
+          <Route exact={true} path="/" component={Landing} />
+          <Route path="/surveys" component={Dashboard} />
+
         </div>
         </BrowserRouter>
 
