cc88c92d14fa438267745c1ff08e8a25c53a0a4c
MichaelWiss <michael.wiss@gmail.com>
2017-08-13 16:29:59 -0500

    imports

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>
@@ -10,7 +12,7 @@ class App extends Component {
 	componentDidMount() {
 
 	}
-	
+
 	render (
 	 return (
        <div className="container">
