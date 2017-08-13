3e8ac8f683b2b7a6f046a9b81bb34d4d1a4fa5c2
MichaelWiss <michael.wiss@gmail.com>
2017-08-13 14:40:02 -0500

    refactor app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {BrowserRouter, Route } from 'react-router-dom';
 
 import Header from './Header';
@@ -6,7 +6,7 @@ const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-const App = () => {
+class App extends Component {
 	return (
        <div className="container">
          
