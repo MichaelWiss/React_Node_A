312272a8a54065d799a455cbeca90e6f2136ced4
MichaelWiss <michael.wiss@gmail.com>
2017-08-18 20:21:01 -0500

    auth

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -21,7 +21,7 @@ class Header extends Component {
 		<nav>
 		  <div className="nav-wrapper">
 		    <Link
-		    to={this.props.user ? '/surveys' : '/'} className="left brand-logo">
+		    to={this.props.auth ? '/surveys' : '/'} className="left brand-logo">
 		      Emaily
 		    </Link>
 		    <ul className="right">
