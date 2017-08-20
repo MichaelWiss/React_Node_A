a25c766ae5c31ac96bf41e7b9ffab4784bf70e49
MichaelWiss <michael.wiss@gmail.com>
2017-08-20 18:17:44 -0500

    import in Header

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -3,11 +3,12 @@ import StripeCheckout from 'react-stripe-checkout';
 
 class Payments extends Component {
 	render() {
+		debugger;
 		return (
           <StripeCheckout 
             amount={500}
             token={token => console.log(token)}
-            stripeKey={}
+            stripeKey={process.env.REACT_APP_STRIPE_KEY}
             />
 		);
 	}
