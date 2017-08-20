c60f2e988ed02f1b2dd6d85d328d41adb3196314
MichaelWiss <michael.wiss@gmail.com>
2017-08-19 19:44:53 -0500

    keys

--- config/dev.js
+++ config/dev.js
@@ -4,7 +4,9 @@ module.exports = {
 	googleClientID: '1015889727661-ib28rije4a4rfclu0c1lfj6mrh5h4nus.apps.googleusercontent.com',
     googleClientSecret: 'OcXOsvpkXJUxi00zzSmWnnwF',
     mongoURI: 'mongodb://72_Knisho:password@ds129023.mlab.com:29023/emaily--dev',
-    cookieKey: 'luablfbalkdbafdkbfadbsfhlgbflshgfbgalfhfuiew38yr894bf'
+    cookieKey: 'luablfbalkdbafdkbfadbsfhlgbflshgfbgalfhfuiew38yr894bf',
+    stripePublishableKey: 'pk_test_tl6MsDYYvhagoYXHHpzDzDP2',
+    stripeSecretKey: 'sk_test_bMv9drWh2KM7WOQoUlciQvvP'
 };
 
 
