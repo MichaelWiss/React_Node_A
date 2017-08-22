31468c16c6b024d8decc1319ac7376ae7e21cd32
MichaelWiss <michael.wiss@gmail.com>
2017-08-21 21:19:12 -0500

    return array in header

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -13,7 +13,9 @@ class Header extends Component {
            	<li><a href="/auth/google">Login With Google</a></li>
            	);
        	default:
-       	return <li><a href="/api/logout">Logout</a></li>;
+       	return [
+       	   <li><a href="/api/logout">Logout</a></li>
+       	   ];
        }
 	}
 	render() {
