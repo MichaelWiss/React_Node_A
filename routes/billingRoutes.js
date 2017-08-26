0fb5fd5304647298f428330ab6a01c29a9609e6c
MichaelWiss <michael.wiss@gmail.com>
2017-08-26 16:24:58 -0500

    stripe charge objecct

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -4,6 +4,11 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
   app.post('/api/stripe', (req, res) => {
-  	onsole.log(req.body);
+  	stripe.charges.create({
+  		amount: 500,
+  		currency: 'usd',
+  		description: '$5 for five credits',
+  		source: req.body.id
+  	})
   });
 };
