7bb02f13fb0e6121d71a05b45164cab3dd31cb77
MichaelWiss <michael.wiss@gmail.com>
2017-08-29 16:49:08 -0500

    show credits in header

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -5,7 +5,6 @@ const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
   app.post('/api/stripe', requireLogin, async (req, res) => {
-  	
   	const charge = await stripe.charges.create({
   		amount: 500,
   		currency: 'usd',
