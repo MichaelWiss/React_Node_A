f3c4a07d50b01c3d263a362772ff03ba36f7a2e2
MichaelWiss <michael.wiss@gmail.com>
2017-09-04 15:09:00 -0500

    mongoose model surveys

--- models/Surveys.js
+++ models/Surveys.js
@@ -3,5 +3,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const surveySchema = new Schema({
-
+   title: String,
+   body: String,
+   subject: String,
+   recipients: [String]
 });
+
+mongoose.model('surveys', surveysSchema);
