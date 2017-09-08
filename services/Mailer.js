6dd6c4d01b8f2ac757304b25757cdafbdf3380cf
MichaelWiss <michael.wiss@gmail.com>
2017-09-08 12:16:13 -0500

    fill out Mailer class

--- services/Mailer.js
+++ services/Mailer.js
@@ -3,6 +3,14 @@ const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
+	constructor({ subject, recipients }, content) {
+		super();
+       
+
+
+       this.from_email = new helper('no-reply@emaily.com');
+       this.subject = subject;
+	}
 
 }
 
