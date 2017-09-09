fee0e9fdd437cea11d02306a90c8da16a2d69870
MichaelWiss <michael.wiss@gmail.com>
2017-09-09 17:26:25 -0500

    email template

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,15 @@
 module.exports = (survey) => {
-	return '<div>' + survey.body + '</div>';
+	return `
+      <html>
+        <body>
+         <div style="text-align: center;">
+         <h3> I'd like you're imput!</h3>
+         <p>Please answer the following question:</p>
+         <p>${survey.body}</p>
+         <div>
+            <a href=""
+         </div>
+        </body>
+      </html>
+	`;
 };
