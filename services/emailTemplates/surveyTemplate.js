1203f3a4d3989cabe7edf4841aae9193b5a2c325
MichaelWiss <michael.wiss@gmail.com>
2017-09-09 17:31:48 -0500

    email templating

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -7,7 +7,10 @@ module.exports = (survey) => {
          <p>Please answer the following question:</p>
          <p>${survey.body}</p>
          <div>
-            <a href=""
+           <a href="http://localhost:3000">Yes</a>
+         </div>
+         <div>
+           <a href="http://localhost:3000">No</a>
          </div>
         </body>
       </html>
