29aca601ebbd9c498780192779df765daa5171c7
MichaelWiss <michael.wiss@gmail.com>
2017-09-13 12:47:59 -0500

    plus

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -6,7 +6,7 @@ const Dashboard = () => {
 		Dashboard
 		<div className="fixed-action-btn">
 		  <a className="btn-floating btn-large red">
-		    <i className="material-icons">add</i>
+		    <i className="material-icons">chat_bubble_outline</i>
 		  </a>
 		</div>
 	  </div>	
