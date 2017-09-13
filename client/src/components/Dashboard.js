53f6df488133723089374215b98a87bee8fa4bc4
MichaelWiss <michael.wiss@gmail.com>
2017-09-13 10:43:01 -0500

    button icon

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 const Dashboard = () => {
 	return (
-		<div>
+	  <div>	
 		Dashboard
 		<div className="fixed-action-btn">
 		  <a className="btn-floating btn-large red">
+		    <i className="material-icons">add</i>
 		  </a>
 		</div>
-		</div>
+	  </div>	
 		);
 };
 
