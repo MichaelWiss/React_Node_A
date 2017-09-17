d474f3e9cb99544722e508920a643e27908f4622
MichaelWiss <michael.wiss@gmail.com>
2017-09-17 18:56:34 -0500

    iteration works

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -13,7 +13,9 @@ const FIELDS = [
 class SurveyForm extends Component {
 	renderFields() {
         return _.map(FIELDS, ({ label, name })=> {
-          return <Field component={SurveyField} type="text" label={field.label}/>
+          return (
+            <Field component={SurveyField} type="text" label={label} name={name} />
+            );
         });  
     
 	}
