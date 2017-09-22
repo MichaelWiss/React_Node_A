7656431f4be0f68407e96d8d070acff7b068fb96
MichaelWiss <michael.wiss@gmail.com>
2017-09-22 14:15:35 -0500

    SurveyNew

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import SurveyForm from'./SurveyForm';
+import SurveyReviewForm from './SurveyReviewForm';
 
 class SurveyNew extends Component {
+	state = { showReviewForm: false };
+
+	renderContent() {
+		if (this.state.showReviewForm === true) {
+            return <SurveyReviewForm />;
+		}
+	}
+
 	render() {
 		return (
 			<div>
