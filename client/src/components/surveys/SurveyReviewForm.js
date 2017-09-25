5b33092d679c4f71c47b5ee424a3be09639e6314
MichaelWiss <michael.wiss@gmail.com>
2017-09-25 12:11:22 -0500

    back button

--- client/src/components/surveys/SurveyReviewForm.js
+++ client/src/components/surveys/SurveyReviewForm.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const SurveyReviewForm = () => {
+const SurveyReviewForm = ({ onCancel }) => {
 	return (
        <div>
          <h5>Please confirm your entries</h5>
+         <button
+           className="yellow darken-3 btn-flat"
+           onClick={onCancel}
+           >
+            Back
+         </button>
        </div>
 		);
 };
